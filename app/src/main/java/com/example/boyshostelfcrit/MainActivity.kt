7b8c52a0810823f430ebb42ae2d50c4591150d27
Ajinkya <deshpande.ajinkya09@gmail.com>
2022-08-15 02:48:55 +0530

    Bottom Navigation Bar created using Meow Navigation Bar

---
+++ app/src/main/java/com/example/boyshostelfcrit/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.boyshostelfcrit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        addFragment(HomeFragment.newInstance())
+        bottomNavigation.show(0)
+        bottomNavigation.add(MeowBottomNavigation.Model(0,R.drawable.ic_home))
+        bottomNavigation.add(MeowBottomNavigation.Model(1,R.drawable.ic_notifications))
+        bottomNavigation.add(MeowBottomNavigation.Model(2,R.drawable.ic_profile))
+
+        bottomNavigation.setOnClickMenuListener {
+            when(it.id){
+                0 -> {
+                    replaceFragment(HomeFragment.newInstance())
+                }
+                1 -> {
+                    replaceFragment(NotificationFragment.newInstance())
+                }
+                2 -> {
+                    replaceFragment(ProfileFragment.newInstance())
+                }
+                else -> {
+                    replaceFragment(HomeFragment.newInstance())
+                }
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment:Fragment){
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.replace(R.id.fragmentContainer,fragment).addToBackStack(Fragment::class.java.simpleName).commit()
+    }
+
+    private fun addFragment(fragment:Fragment){
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.add(R.id.fragmentContainer,fragment).addToBackStack(Fragment::class.java.simpleName).commit()
+    }
+}

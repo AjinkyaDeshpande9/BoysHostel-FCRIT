b485294a2cc017e0e4ed1e7a1a393f32b893ad42
Ajinkya <deshpande.ajinkya09@gmail.com>
2022-08-19 01:12:46 +0530

    Home and Profile Fragment Resources

---
+++ app/src/main/java/com/example/boyshostelfcrit/FormShort.kt
@@ -0,0 +1,11 @@
+package com.example.boyshostelfcrit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormShort : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_short)
+    }
+}

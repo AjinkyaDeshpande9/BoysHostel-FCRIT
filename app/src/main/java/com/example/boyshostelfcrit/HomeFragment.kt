dd0594eab91ea93c35ce7d940b3ecc9d25c1a979
Ajinkya <deshpande.ajinkya09@gmail.com>
2022-08-19 01:12:19 +0530

    Home and Profile Fragment XML Design partially completed

--- app/src/main/java/com/example/boyshostelfcrit/HomeFragment.kt
+++ app/src/main/java/com/example/boyshostelfcrit/HomeFragment.kt
@@ -1,18 +1,54 @@
 package com.example.boyshostelfcrit
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
+
+    public fun clickListener(){
+        formShortDuration?.setOnClickListener { openFormShortActivity() }
+        formLongDuration?.setOnClickListener { openFormLongActivity() }
+    }
+
+    public fun openFormShortActivity() {
+        activity?.let{
+            val intent = Intent (it, FormShort::class.java)
+            it.startActivity(intent)
+        }
+    }
+
+    public fun openFormLongActivity(){
+        activity?.let{
+        val intent = Intent (it, FormLong::class.java)
+        it.startActivity(intent)
+    }
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+
+
+//        formLongDuration.setOnClickListener {
+//            val intent = Intent (getActivity(), FormLong::class.java)
+//            getActivity()?.startActivity(intent)
+//        }
+//        formShortDuration.setOnClickListener {
+//            val intent = Intent (getActivity(), FormShort::class.java)
+//            getActivity()?.startActivity(intent)
+//        }
+        clickListener()
         return inflater.inflate(R.layout.fragment_home, container, false)
+
     }
 
     companion object {
@@ -21,4 +57,6 @@ class HomeFragment : Fragment() {
                     arguments = Bundle().apply {}
                 }
     }
+
+
 }

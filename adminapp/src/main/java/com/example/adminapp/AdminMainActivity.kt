cc94c7c87ea391e37ae0c128f6457b6c105fc0f3
Ajinkya <deshpande.ajinkya09@gmail.com>
2022-09-06 23:17:51 +0530

    Admin App module updated

---
+++ adminapp/src/main/java/com/example/adminapp/AdminMainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.adminapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdminMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

96b43e02eee2341448b84c8a1a82af76b9193ed7
Ajinkya <deshpande.ajinkya09@gmail.com>
2022-10-07 23:02:28 +0530

    Form Long Duration Confirm Page Ui Update (App)

--- app/src/main/java/com/example/boyshostelfcrit/home/FormLong.kt
+++ app/src/main/java/com/example/boyshostelfcrit/home/FormLong.kt
@@ -5,55 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.boyshostelfcrit.HomeFragment
 import com.example.boyshostelfcrit.R
+import com.example.boyshostelfcrit.databinding.FragmentFormLongBinding
+import com.example.boyshostelfcrit.databinding.FragmentProfileBinding
+import com.example.boyshostelfcrit.profile.LeaveLogs
+import com.example.boyshostelfcrit.profile.PersonalDetails
+import kotlinx.android.synthetic.main.fragment_home.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FormLong.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FormLong : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_form_long, container, false)
+        val formlongconfirm = FormLongConfirm()
+        val bind = FragmentFormLongBinding.inflate(layoutInflater)
+
+        bind.buttonContinue.setOnClickListener {
+            fragmentManager?.beginTransaction()?.apply {
+                replace(
+                    R.id.mainFragmentContainer,
+                    formlongconfirm,
+                    FormLongConfirm::class.java.simpleName
+                )
+                    .addToBackStack(null)
+                    .commit()
+            }
+            bind.longFormBack.setOnClickListener {
+                fragmentManager?.beginTransaction()?.apply {
+                    replace(
+                        R.id.mainFragmentContainer, HomeFragment(), HomeFragment::class.java.simpleName)
+                        .addToBackStack(null)
+                        .commit()
+                }
+            }
+
+        }
+        return bind.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FormLong.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance() =
             FormLong().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
                 }
             }
     }
